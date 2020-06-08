adf8472e722a9072e81e536a5460dd60778480b4
Rafael Xavier <rxaviermontero@gmail.com>
2020-06-08 01:49:53 -0300

    Even better

--- econuy/app/tasks.py
+++ econuy/app/tasks.py
@@ -1,4 +1,3 @@
-import warnings
 from typing import Union, List
 
 from sqlalchemy.engine.base import Connection, Engine
@@ -10,13 +9,13 @@ def full_update(con: Union[Connection, Engine],
     output = []
     for f in functions:
         try:
-            print(f"Running {f.__module__}...")
+            print(f"Running {f.__name__} in {f.__module__}...")
             output.append(f(update_loc=con, save_loc=con, **kwargs))
             print("Success.")
         except:
             # This exception is intentionally broad.
             # If any function fails, carry on with the next one.
-            warnings.warn(f"{f.__module__} failed.")
+            print(f"{f.__name__} in {f.__module__} failed.")
             continue
     return output
 
