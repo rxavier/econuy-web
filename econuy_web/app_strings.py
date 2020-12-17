852b8959a68d6f5154cffc2456e90fc491dcee36
Rafael Xavier <rxaviermontero@gmail.com>
2020-12-17 10:39:01 -0300

    Update dropdown

--- econuy_web/app_strings.py
+++ econuy_web/app_strings.py
@@ -77,5 +77,14 @@ table_options = {
     "global_stocks": "Índices bursátiles de EE.UU., Unión Europea, Japón y China (30-dic-27, D)",
     "global_policy_rates": "Tasas de interés de política monetaria de EE.UU., Eurozona, Japón y China (01-ene-46, D)",
     "global_long_rates": "Tasas de interés de largo plazo (10 años) de EE.UU., Alemania, Francia, España, Italia, Reino Unido, Japón y China (02-ene-62, D)",
-    "global_currencies": "Cotización de monedas de EE.UU., Eurozona, Japón y China (04-ene-71, D)"
+    "global_currencies": "Cotización de monedas de EE.UU., Eurozona, Japón y China (04-ene-71, D)",
+    "regional_gdp": "PBI real desestacionalizado de Argentina y Brasil (mar-93, T)",
+    "regional_monthly_gdp": "PBI real mensual desestacionalizaod de Argentina y Brasil (ene-03, M)",
+    "regional_cpi": "Índice de precios al consumo de Argentina y Brasil (ene-70, M)",
+    "regional_nxr": "Tipo de cambio nominal diario de Argentina y Brasil (9-abr-02, D)",
+    "regional_rxr": "Tipo de cambio real vs. EE.UU. (dic-70, M)",
+    "regional_embi_spreads": "EMBI spread (JP Morgan) de Argentina y Brasil (11-dic-98, D)",
+    "regional_embi_yields": "EMBI yield (JP Morgan) de Argentina y Brasil (11-dic-98, D)",
+    "regional_stocks": "Índices bursátiles en USD de Argentina y Brasil (9-abr-02, D)",
+    "regional_policy_rates": "Tasas de interés de política monetaria de Argentina y Brasil (4-jun-86, D)"
 }
