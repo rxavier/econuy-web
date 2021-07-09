cf995d53255fbec8634ce8f8a74797e6f0b77923
Rafael Xavier <rxaviermontero@gmail.com>
2021-07-09 14:52:06 -0300

    Make updating more flexible

--- econuy_web/update.py
+++ econuy_web/update.py
@@ -1,3 +1,5 @@
+import sys
+
 from econuy.session import Session
 
 from econuy_web import db, create_app
@@ -6,5 +8,9 @@ if __name__ == "__main__":
     app = create_app()
     app.app_context().push()
     s = Session(location=db.engine)
-    s.get_bulk("all")
-    s.get("_lin_gdp")
+    if len(sys.argv) == 1:
+        s.get_bulk("all")
+        s.get("_lin_gdp")
+    else:
+        for arg in sys.argv[1:]:
+            s.get(arg)
