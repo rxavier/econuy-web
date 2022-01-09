a0672ac4c587d7d57edb84cd4a12bb5c01b02c83
Rafael Xavier <rxaviermontero@gmail.com>
2022-01-09 12:19:51 -0300

    Remove unused function

--- econuy_web/tasks.py
+++ econuy_web/tasks.py
@@ -1,43 +1,8 @@
 from pathlib import Path
-from typing import Union, List
+from typing import Union
 
 from sqlalchemy.engine.base import Connection, Engine
 from sqlalchemy import inspect
-
-
-def full_update(
-    con: Union[Connection, Engine], functions: List, run: int = 1, output: List = None
-) -> List:
-    print(f"*** RUN: {run} ***")
-    if output is None:
-        output = []
-    failed = []
-    for f in functions:
-        try:
-            name = f.__name__
-            module = f.__module__
-        except AttributeError:
-            name = f.func.__name__
-            module = f.func.__module__
-        try:
-            print(f"{module}.{name}...")
-            output.append(f(update_loc=con, save_loc=con))
-            print("---SUCCESS---")
-        except:
-            # This exception is intentionally broad.
-            # If any function fails, carry on with the next one.
-            print("---FAIL---")
-            failed.append(f)
-            continue
-    if len(failed) > 0 and run < 3:
-        run += 1
-        return full_update(con=con, functions=failed, run=run, output=output)
-    if len(failed) > 0:
-        failed_text = f" {len(failed)} datasets could not be updated."
-    else:
-        failed_text = ""
-    print(f"Updating required {run} run(s).{failed_text}")
-    return output
 
 
 def clear_tables(con: Union[Connection, Engine]):
